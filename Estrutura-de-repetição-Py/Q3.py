acb4eb9d740ac91a2a822f1947a15f50caccce4a
Pedro Henrique <Ph9084533@gmail.com>
2025-02-11 17:39:30 +0000

    Completando questão 3 de programação

---
+++ Estrutura-de-repetição-Py/Q3.py
@@ -0,0 +1,8 @@
+# Faça um programa que solicite ao usuário dez números e
+# imprima a quantidade de números pares e a quantidade dos
+# números ímpares.
+
+usuario = int(input("Informe no total de 10 números:"))
+
+for i in range(usuario):
+    
